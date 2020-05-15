778fb8d44a6303ec781e81318f34960132edc39c
Nathan Thomas <nate.p.thomas@gmail.com>
2020-05-15 09:14:58 -0400

    Initial commit

---
+++ src/main/java/com/nthomas/springbootwithcitrus/SpringBootWithCitrusApplication.java
@@ -0,0 +1,13 @@
+package com.nthomas.springbootwithcitrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithCitrusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithCitrusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nthomas/springbootwithcitrus/SpringBootWithCitrusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nthomas.springbootwithcitrus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithCitrusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
